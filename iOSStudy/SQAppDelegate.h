1f1ba01cf7421be6e55780b7e9e29aac0e3e5eac
shuqiong <toshuqiong@163.com>
2014-08-13 14:19:04 +0800

    add repo to remote

---
+++ iOSStudy/SQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SQAppDelegate.h
+//  iOSStudy
+//
+//  Created by shuqiong on 8/13/14.
+//  Copyright (c) 2014 shuqiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSStudy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSStudy
+//
+//  Created by shuqiong on 8/13/14.
+//  Copyright (c) 2014 shuqiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SQAppDelegate class]));
+    }
+}
